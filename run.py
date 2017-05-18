6e4e96329cc847974428c0342da8ee4521daf258
pdonorio <tempo.pause@gmail.com>
2017-05-18 15:08:48 +0200

    Adding docker.py

--- run.py
+++ run.py
@@ -12,17 +12,28 @@ from do.utils.logs import get_logger
 from dockerfile_parse import DockerfileParser
 # https://github.com/DBuildService/dockerfile-parse
 
+import docker
+# https://docker-py.readthedocs.io/en/stable/
+
 ##########################
-SOME_FILE = "/Users/projects/tmp/rapydo/containers/debug.yml"
+SOME_FILE = "/Users/projects/tmp/custom/containers/debug.yml"
 log = get_logger(__name__)
 
 ##########################
+# Use docker client
+client = docker.from_env()
+containers = client.containers.list()
+log.debug("Docker:%s" % containers)
+
+##########################
+# Read YAML files
 compose = load_yaml_file(SOME_FILE)
 services_data = compose.get('services', {})
 services_list = list(services_data.keys())
 log.debug("Services:\n%s" % services_list)
 
 ##########################
+# Parse docker files
 dfp = DockerfileParser()
 # dfp = DockerfileParser('Dockerfile')
 # dfp.content
