68a432d6d317ca2736c04614465eac7391a74917
pdonorio <tempo.pause@gmail.com>
2017-05-27 15:20:46 +0200

    Adding a connection check utility

--- do/__init__.py
+++ do/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import requests
 
 FRAMEWORK_NAME = 'RAPyDo'
 
@@ -10,10 +11,17 @@ CONTAINERS_YAML_DIRNAME = 'containers'
 PROJECT_YAML_SPECSDIR = 'specs'
 PROJECT_CONF_FILENAME = 'project_configuration'
 
-# FIXME: @packaging
+# FIXME: @packaging ?
 ABSOLUTE_PATH = os.path.dirname(os.path.realpath(__file__)) + "/.."
 
 containers_yaml_path = os.path.join(os.curdir, CONTAINERS_YAML_DIRNAME)
 project_specs_yaml_path = os.path.join(os.curdir, PROJECT_YAML_SPECSDIR)
 
-defaults_path = os.path.join('specs')
+
+def check_internet(test_site='https://www.google.com'):
+    try:
+        requests.get(test_site)
+    except requests.ConnectionError:
+        return False
+    else:
+        return True
