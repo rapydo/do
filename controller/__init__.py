389e73edac3b2c8466cadd673f1d266cbbe256db
pdonorio <tempo.pause@gmail.com>
2017-07-15 23:54:55 +0200

    Bumping to 0.5.1 to make sync of all packages

--- controller/__init__.py
+++ controller/__init__.py
@@ -2,15 +2,15 @@
 
 import sys
 
-# NOTE: telling the app if testing or not
-# http://j.mp/2uifoza
-
-TESTING = hasattr(sys, '_called_from_test')
+__version__ = '0.5.1'
 
 FRAMEWORK_NAME = 'RAPyDo'
-__version__ = '0.5.0'
-
 # PROJECT_YAML_SPECSDIR = 'specs'
 COMPOSE_ENVIRONMENT_FILE = '.env'
 SUBMODULES_DIR = 'submodules'
 PLACEHOLDER = '#@$%-REPLACE-#@%$-ME-#@$%'
+
+##################
+# NOTE: telling the app if testing or not
+# http://j.mp/2uifoza
+TESTING = hasattr(sys, '_called_from_test')
