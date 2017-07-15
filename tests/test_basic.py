9ee352cd26423a5e6c62203330370e0aa2c4bb91
Mattia D'Antonio <m.dantonio@cineca.it>
2017-07-15 16:35:11 +0200

    First check of application output

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,46 +1,51 @@
 
+
+from io import StringIO
+import logging
 from controller.arguments import ArgParser
 from controller.app import Application
 from utilities.logs import get_logger
 log = get_logger(__name__)
 
 
+def capture_loggers():
+    log_capture_string = StringIO()
+    from controller.app import log
+    capture_logger(log, log_capture_string)
+
+    return log_capture_string
+
+
+def capture_logger(log, output_string):
+    ch = logging.StreamHandler(output_string)
+    # ch.setLevel(logging.DEBUG)
+    log.addHandler(ch)
+
+
+def exec_command(command):
+    log_capture_string = capture_loggers()
+    command = command.split(" ")
+    arguments = ArgParser(args=command)
+
+    try:
+        app = Application(arguments)
+    # NOTE: docker-compose calls SystemExit at the end of the command...
+    except SystemExit:
+        log.info('completed')
+
+    log_contents = log_capture_string.getvalue().split("\n")
+    log_capture_string.close()
+
+    return app, log_contents
+
+
 def test_do():
-    arguments = ArgParser(
-        args=['/usr/local/bin/rapydo', 'init']
-    )
-    try:
-        Application(arguments)
-    # NOTE: docker-compose calls SystemExit at the end of the command...
-    except SystemExit:
-        log.info('completed')
 
-    arguments = ArgParser(
-        args=['/usr/local/bin/rapydo', 'update']
-    )
-    try:
-        Application(arguments)
-    # NOTE: docker-compose calls SystemExit at the end of the command...
-    except SystemExit:
-        log.info('completed')
+    _, log_contents = exec_command("rapydo init")
+    assert log_contents[-1] == "Project initialized"
 
-    arguments = ArgParser(
-        args=['/usr/local/bin/rapydo', 'start']
-    )
+    _, _ = exec_command("rapydo update")
+    _, _ = exec_command("rapydo start")
+    _, _ = exec_command("rapydo remove")
 
-    try:
-        Application(arguments)
-    # NOTE: docker-compose calls SystemExit at the end of the command...
-    except SystemExit:
-        log.info('completed')
-
-    arguments = ArgParser(
-        args=['/usr/local/bin/rapydo', 'remove']
-    )
-
-    try:
-        Application(arguments)
-    # NOTE: docker-compose calls SystemExit at the end of the command...
-    except SystemExit:
-        log.info('completed')
     assert True
