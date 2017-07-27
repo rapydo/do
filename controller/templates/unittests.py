ada2f7a880dbcf1b2d27fb76ed0ec04018238e97
pdonorio <tempo.pause@gmail.com>
2017-07-27 15:24:51 +0200

    better unittests

--- controller/templates/unittests.py
+++ controller/templates/unittests.py
@@ -21,26 +21,31 @@ class Test{{ class_name }}(RestTestsAuthenticatedBase):
 
     _main_endpoint = '/{{ endpoint_name }}'
 
+    def test_01_GET_giveityourname(self):
+        """
+        testing a feature on your endpoint class
+        """
+
+        endpoint = (self._api_uri + self._main_endpoint)
+        log.info('*** Testing GET call on %s' % endpoint)
+
+        r = self.app.get(endpoint)  # If NO authorization required
+        # r = self.app.get(
+        #     endpoint,
+        #     headers=self.__class__.auth_header  # If authorization required
+        # )
+
+        # Assert what is right or wrong
+        self.assertEqual(r.status_code, self._hcodes.HTTP_OK_BASIC)
+        data = json.loads(r.get_data(as_text=True))
+
+        # pretty print data obtained from API to check the content
+        # log.pp(data)
+        self.assertEqual(data['Response']['data'], 'Hello world!')
+
     # def tearDown(self):
     #     """ override original teardown if you create custom data """
 
     #     log.debug('### Cleaning custom data ###\n')
     #     super().tearDown()
 
-    def test_01_GET_public_PID(self):
-        """ Test directory creation: POST """
-
-        endpoint = (self._api_uri + self._main_endpoint)
-        log.info('*** Testing GET call on %s' % endpoint)
-
-        # If NO authorization required
-        r = self.app.get(endpoint, headers=self.__class__.auth_header)
-        # If authorization required
-        # r = self.app.get(endpoint, headers=self.__class__.auth_header)
-
-        # Assert what is right or wrong
-        self.assertEqual(r.status_code, self._hcodes.HTTP_OK_BASIC)
-        data = json.loads(r.get_data(as_text=True))
-        # pretty print data obtained from API to check the content
-        # log.pp(data)
-        self.assertEqual(data['Response']['data'], 'Hello world!')
