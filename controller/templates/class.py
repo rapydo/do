273895a50db5ee11179d58c1ba6e7be26e9e433f
pdonorio <tempo.pause@gmail.com>
2017-07-27 12:03:23 +0200

    typos chars

--- controller/templates/class.py
+++ controller/templates/class.py
@@ -5,21 +5,21 @@ Endpoint example for the RAPyDo framework
 """
 
 #################
-# IMPORTS
+# IMPORTS
 from restapi.rest.definition import EndpointResource
 # from restapi.services.detect import detector
 from utilities.logs import get_logger
 
 #################
-# INIT VARIABLES
+# INIT VARIABLES
 log = get_logger(__name__)
 service_name = "{{ service_name }}"
-# NOTE: if you need to operate based on service availability
+# NOTE: if you need to operate based on service availability
 # SERVICE_AVAILABLE = detector.check_availability(service_name)
 
 
 #################
-# REST CLASS
+# REST CLASS
 class {{ class_name }}(EndpointResource):
 
     def get(self):
@@ -30,3 +30,4 @@ class {{ class_name }}(EndpointResource):
         log.debug("Connected to %s:\n%s" % (service_name, service_handle))
 
         return self.force_response(response)
+
