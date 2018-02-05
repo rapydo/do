072783e11960db63ef991c981466a1ff5c4b320a
Mattia <m.dantonio81@gmail.com>
2018-02-05 10:55:15 +0100

    Fix msg level on tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -52,13 +52,13 @@ def test_all(capfd):
     _, err = exec_command(capfd, "rapydo init")
     log.pp(err)
     assert env_log_prefix_verbose in err
-    assert "INFO Frontend libs not found, will be installed at startup" in err
+    assert "WARNING Frontend libs not found, will be installed at startup" in err
     assert "INFO Project initialized" in err
 
     # UPDATE on rapydo-core
     _, err = exec_command(capfd, "rapydo update")
     assert env_log_prefix_verbose in err
-    assert "INFO Frontend libs not found, will be installed at startup" in err
+    assert "WARNING Frontend libs not found, will be installed at startup" in err
     assert "INFO All updated" in err
 
     # _, err = exec_command(capfd, "rapydo build")
