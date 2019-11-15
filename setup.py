996a2db3924e375db6f8babced21f77ba402073f
Mattia <m.dantonio81@gmail.com>
2019-11-15 16:12:00 +0100

    Added missing dependency jinja2

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "pytz",
         "loguru",
         "better_exceptions",
+        "jinja2",
         "parse_it==3.3.2",
         # "requests==2.18.4",
         # "requests==2.20.0",
