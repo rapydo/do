e930175e6c299b48fcea47a30d52c68b6bcca582
Mattia <m.dantonio81@gmail.com>
2020-05-08 16:22:59 +0200

    Added custom footer templates and requirement

---
+++ controller/templates/custom.footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'customfooter',
+  providers: [],
+  templateUrl: './custom.footer.html',
+})
+export class CustomFooterComponent {
+
+  constructor() { }
+
+}
