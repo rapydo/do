fc89a2faaaca4eacd49a661fbced40ae032c21db
Mattia <m.dantonio81@gmail.com>
2020-06-02 08:36:01 +0200

    Removed unnecessary log

--- controller/commands/check.py
+++ controller/commands/check.py
@@ -105,8 +105,6 @@ def __call__(args, base_services, compose_config, active_services, gits, **kwarg
             for image_tag, build in builds.items():
 
                 if image_tag not in dimages:
-                    # Missing images will be created at startup
-                    log.info("Image {} is missing", image_tag)
                     continue
 
                 if not any(x in active_services for x in build['services']):
