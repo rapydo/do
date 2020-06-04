d42f0e60cfe05e1aba6a1a1e8df08f00edc3dbbf
Mattia <m.dantonio81@gmail.com>
2020-06-04 09:10:49 +0200

    Bump docker-compose from 1.25.5 to 1.26.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         ],
     },
     install_requires=[
-        "docker-compose==1.25.5",
+        "docker-compose==1.26.0",
         "dockerfile-parse",
         "python-dateutil",
         "pytz",
