23c3c4a76e451281ae6b16506838c2fb62cae682
Mattia <m.dantonio81@gmail.com>
2020-07-10 18:44:29 +0200

    # Tests fix

--- tests/test_libs.py
+++ tests/test_libs.py
@@ -54,6 +54,7 @@ def test_all(capfd):
     assert "main" not in values
 
     os.unlink(".rapydo")
+    app = Application()
     values = app.autocomplete_service("")
     assert len(values) == 0
     values = app.autocomplete_allservice("")
