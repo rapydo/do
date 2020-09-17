6f1249eb5fe448271e56f1593e44bb013031214e
Mattia <m.dantonio81@gmail.com>
2020-09-17 09:43:43 +0200

    Added --all flag to rapydo pull to include custom images

--- controller/commands/pull.py
+++ controller/commands/pull.py
@@ -1,26 +1,38 @@
+import typer
+
 from controller import log
 from controller.app import Application, Configuration
 from controller.compose import Compose
 
 
 @Application.app.command(help="Pull available images from docker hub")
-def pull():
+def pull(
+    include_all: bool = typer.Option(
+        False,
+        "--all",
+        help="Include both core and custom images",
+        show_default=False,
+    )
+):
     Application.controller.controller_init()
 
-    dc = Compose(files=Application.data.base_files)
+    if include_all:
+        dc = Compose(files=Application.data.files)
+        services_intersection = Application.data.services
+    else:
+        dc = Compose(files=Application.data.base_files)
+        base_services_list = []
+        for s in Application.data.base_services:
+            base_services_list.append(s.get("name"))
 
-    base_services_list = []
-    for s in Application.data.base_services:
-        base_services_list.append(s.get("name"))
-
-    if Configuration.services_list:
-        for s in Application.data.services:
-            if s not in base_services_list:
-                log.exit("Invalid service name: {}", s)
-    # List of BASE active services (i.e. remove services not in base)
-    services_intersection = list(
-        set(Application.data.services).intersection(base_services_list)
-    )
+        if Configuration.services_list:
+            for s in Application.data.services:
+                if s not in base_services_list:
+                    log.exit("Invalid service name: {}", s)
+        # List of BASE active services (i.e. remove services not in base)
+        services_intersection = list(
+            set(Application.data.services).intersection(base_services_list)
+        )
 
     options = {
         "SERVICE": services_intersection,
