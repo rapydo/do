e48c94c5ec4cfbe1915e4606d02be2e166532b12
Mattia <m.dantonio81@gmail.com>
2020-12-11 10:43:51 +0100

    Added Faker to test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,17 @@
 import os
 
-os.makedirs("rapydo_tests/data/logs")
-os.chdir("rapydo_tests")
+import pytest
+from faker import Faker
+
+f = Faker("en_US")
+
+folder = f.file_name(extension="")
+os.makedirs(f"{folder}/data/logs")
+os.chdir(folder)
+
+print(folder)
+
+
+@pytest.fixture
+def fake():
+    return f
