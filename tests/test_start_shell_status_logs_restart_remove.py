6e60d66653fc4e4236747c781015521499b79f6a
Mattia <m.dantonio81@gmail.com>
2021-03-16 11:46:46 +0100

    # Tests fix /5

--- tests/test_start_shell_status_logs_restart_remove.py
+++ tests/test_start_shell_status_logs_restart_remove.py
@@ -161,7 +161,8 @@ def test_all(capfd: Capture) -> None:
         "-s frontend,backend logs --tail 10 --no-color",
         "docker-compose command: 'logs'",
         # Logs are prefixed because more than one service is shown
-        "backend_1       | Development mode",
+        "backend_1       | Testing mode",
+        # "backend_1       | Development mode",
         "frontend_1      | Merging files...",
     )
 
