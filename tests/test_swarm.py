3e20e58318549d374ff2f42ba9e83266d571b1fc
Mattia <m.dantonio81@gmail.com>
2021-06-06 19:16:35 +0200

    # Debug code

--- tests/test_swarm.py
+++ tests/test_swarm.py
@@ -96,6 +96,11 @@ def test_swarm(capfd: Capture) -> None:
 
     exec_command(
         capfd,
+        "list env",
+    )
+
+    exec_command(
+        capfd,
         "start --force",
         "Force flag is not yet implemented",
         "Stack started",
