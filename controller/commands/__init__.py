cd4a1bf93829c6585c03f4ae746cde429f085047
Mattia <m.dantonio81@gmail.com>
2021-06-13 10:01:20 +0200

    Commands partitioned between compose, swarm and general categories

--- controller/commands/__init__.py
+++ controller/commands/__init__.py
@@ -7,10 +7,15 @@ from controller import SWARM_MODE
 def load_commands() -> None:
 
     commands_folder = Path(__file__).resolve().parent
+    compose_folder = commands_folder.joinpath("compose")
     swarm_folder = commands_folder.joinpath("swarm")
 
     for c in commands_folder.glob("[!_|.]*.py"):
-        if SWARM_MODE and swarm_folder.joinpath(c.stem).with_suffix(".py").exists():
+        import_module(f"controller.commands.{c.stem}")
+
+    if SWARM_MODE:
+        for c in swarm_folder.glob("[!_|.]*.py"):
             import_module(f"controller.commands.swarm.{c.stem}")
-        else:
-            import_module(f"controller.commands.{c.stem}")
+    else:
+        for c in compose_folder.glob("[!_|.]*.py"):
+            import_module(f"controller.commands.compose.{c.stem}")
