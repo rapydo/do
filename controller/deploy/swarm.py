bd2249dbdaaba0d3f4fb45085e8d59255bc6ea8d
Mattia <m.dantonio81@gmail.com>
2021-06-16 18:58:30 +0200

    # Bug fix

--- controller/deploy/swarm.py
+++ controller/deploy/swarm.py
@@ -15,10 +15,11 @@ from controller.utilities import system
 
 class Swarm:
     def __init__(self, check_initialization: bool = True):
-        if check_initialization and not self.get_token():
-            Application.exit("Swarm is not initialized, please execute rapydo init")
 
         self.docker = Docker().client
+
+        if check_initialization and not self.get_token():
+            Application.exit("Swarm is not initialized, please execute rapydo init")
 
     def init(self) -> None:
         self.docker.swarm.init()
