0616c19217a06a50de0f51f76d198c8f55547303
Mattia <m.dantonio81@gmail.com>
2021-08-05 06:45:56 +0200

    Added registry and images tests

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -3,7 +3,7 @@ This module will test the registry service
 """
 import random
 
-from tests import Capture, create_project
+from tests import Capture, create_project, exec_command
 
 
 def test_swarm_multi_host(capfd: Capture) -> None:
@@ -24,8 +24,63 @@ def test_swarm_multi_host(capfd: Capture) -> None:
         name="swarm",
         auth=auth,
         frontend="no",
-        services=["rabbit", "redis"],
+        # services=["rabbit", "redis"],
     )
 
-    # This is a temporary command and will probably be merged
-    #  with interfaces and volatile commands in a near future
+    exec_command(
+        capfd,
+        "-e HEALTHCHECK_INTERVAL=1s -e SWARM_MANAGER_ADDRESS=127.0.0.1 init",
+        "Initializing Swarm with manager IP 127.0.0.1",
+        "Swarm is now initialized",
+        "Project initialized",
+    )
+
+    exec_command(
+        capfd,
+        "-s backend pull",
+        "Registry 127.0.0.1:5000 not reachable. You can start it with rapydo registry",
+    )
+
+    exec_command(
+        capfd,
+        "-s backend build",
+        "Registry 127.0.0.1:5000 not reachable. You can start it with rapydo registry",
+    )
+
+    exec_command(
+        capfd,
+        "-s backend start",
+        "Registry 127.0.0.1:5000 not reachable. You can start it with rapydo registry",
+    )
+
+    exec_command(
+        capfd,
+        "images",
+        "Registry 127.0.0.1:5000 not reachable. You can start it with rapydo registry",
+    )
+
+    exec_command(
+        capfd,
+        "registry",
+        "This is a temporary command and will probably be merged"
+        " with interfaces and volatile commands in a near future",
+        "Creating registry",
+    )
+    exec_command(
+        capfd,
+        "images",
+        "This registry contains no images",
+    )
+
+    exec_command(
+        capfd,
+        "-s backend pull",
+        "Base images pulled from docker hub and pushed into the local registry",
+    )
+
+    exec_command(
+        capfd,
+        "images",
+        "This registry contains 1 image(s):",
+        "rapydo/backend",
+    )
