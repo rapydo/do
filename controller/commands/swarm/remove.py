ab4330b864ab3972758536358824029219abbda3
Mattia <m.dantonio81@gmail.com>
2021-08-21 09:35:07 +0200

    Added a sleep after remove in swarm mode to let the command to be chained with a ollowing start command

--- controller/commands/swarm/remove.py
+++ controller/commands/swarm/remove.py
@@ -1,3 +1,4 @@
+import time
 from typing import Dict, List, Union
 
 import typer
@@ -24,6 +25,10 @@ def remove(
 
     if all_services:
         swarm.remove()
+        # This sleep is added to add a delay before completing the command
+        # This way even if swarm remove is asynchronous, the command can be
+        # chained with a start command without raising errors
+        time.sleep(1)
         log.info("Stack removed")
     else:
 
