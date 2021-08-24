7ad1dbb9d18ae67673b3e0fe946b43655cf1438f
Mattia <m.dantonio81@gmail.com>
2021-08-24 15:33:54 +0200

    Restart command reimplemented with compose v2

--- controller/commands/compose/restart.py
+++ controller/commands/compose/restart.py
@@ -1,17 +1,13 @@
-from typing import List
-
-import typer
-
 from controller import log
 from controller.app import Application
-from controller.deploy.compose import Compose
+from controller.deploy.compose_v2 import Compose
 
 
 @Application.app.command(help="Restart modified running containers")
 def restart() -> None:
     Application.get_controller().controller_init()
 
-    dc = Compose(files=Application.data.files)
+    dc = Compose(Application.data.files)
     dc.start_containers(Application.data.services)
 
     log.info("Stack restarted")
