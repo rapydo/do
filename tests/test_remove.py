44cdb2aa1b42c7516c010aa2c428faba23dfec5d
Mattia <m.dantonio81@gmail.com>
2021-08-29 07:36:03 +0200

    # Improved remove tests

--- tests/test_remove.py
+++ tests/test_remove.py
@@ -1,6 +1,9 @@
 """
 This module will test the remove command
 """
+from typing import List
+
+from python_on_whales import docker
 
 from controller import SWARM_MODE
 from tests import (
@@ -12,6 +15,28 @@ from tests import (
     start_project,
     start_registry,
 )
+
+
+def get_containers() -> List[str]:
+    containers: List[str] = []
+
+    for container in docker.container.list():
+        name = container.name
+        # this is swarm mode:
+        # project_service.slot.id
+        if "." in name:
+            name = name[0 : name.index(".")]
+        # this is compose mode:
+        # project_service_slot
+        else:
+            index_of_second_underscore = name.index("_", name.index("_") + 1)
+            name = name[0:index_of_second_underscore]
+
+        # in both cases now name is:
+        # project_service
+
+    # Return the containers sorted by name
+    return sorted(containers)
 
 
 def test_remove(capfd: Capture) -> None:
@@ -42,7 +67,16 @@ def test_remove(capfd: Capture) -> None:
             "Stack removed",
         )
 
+    NONE: List[str] = []
+    # BACKEND_ONLY  = ["rem_backend"]
+    POSTGRES_ONLY = ["rem_postgres"]
+    ALL = ["rem_backend", "rem_postgres"]
+
+    assert get_containers() == NONE
+
     start_project(capfd)
+
+    assert get_containers() == ALL
 
     if SWARM_MODE:
         # In swarm mode remove single service is equivalent to scale 0
@@ -54,11 +88,15 @@ def test_remove(capfd: Capture) -> None:
             "Services removed",
         )
 
+        assert get_containers() == POSTGRES_ONLY
+
         exec_command(
             capfd,
             "restart",
             "Stack restarted",
         )
+
+        assert get_containers() == ALL
 
         exec_command(
             capfd,
@@ -66,6 +104,7 @@ def test_remove(capfd: Capture) -> None:
             "Stack removed",
         )
 
+        assert get_containers() == NONE
     else:
 
         exec_command(
@@ -74,11 +113,15 @@ def test_remove(capfd: Capture) -> None:
             "Stack removed",
         )
 
+        assert get_containers() == NONE
+
         exec_command(
             capfd,
             "remove --all backend",
             "Stack removed",
         )
+
+        assert get_containers() == NONE
 
         exec_command(
             capfd, "remove --all", "--all option not implemented yet", "Stack removed"
