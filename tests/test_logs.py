4163aa65876c8222ef527b48f7f9223163f84134
Mattia <m.dantonio81@gmail.com>
2021-08-31 06:20:53 +0200

    # Debug code

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -43,21 +43,20 @@ def test_all(capfd: Capture) -> None:
         "No such service: invalid",
     )
 
-    if not SWARM_MODE:
-        now = datetime.now()
-        timestamp = now.strftime("%Y-%m-%dT")
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%dT")
 
-        signal.signal(signal.SIGALRM, mock_KeyboardInterrupt)
-        signal.alarm(5)
-        # Here using main services option
-        exec_command(
-            capfd,
-            "logs --tail 10 --follow backend",
-            "REST API backend server is ready to be launched",
-        )
-        end = datetime.now()
+    signal.signal(signal.SIGALRM, mock_KeyboardInterrupt)
+    signal.alarm(5)
+    # Here using main services option
+    exec_command(
+        capfd,
+        "logs --tail 10 --follow backend",
+        "REST API backend server is ready to be launched",
+    )
+    end = datetime.now()
 
-        assert (end - now).seconds >= 2
+    assert (end - now).seconds >= 2
 
     exec_command(
         capfd,
