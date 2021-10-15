dfe5dc59538a9d2a5f055383709cafcb2ed7c653
Mattia <m.dantonio81@gmail.com>
2021-10-15 07:04:15 +0200

    # Tests compatibility fix

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -4,7 +4,6 @@ This module will test the interfaces command
 from faker import Faker
 
 from controller import SWARM_MODE, __version__, colors
-from controller.deploy.docker import Docker
 from tests import (
     Capture,
     create_project,
@@ -52,8 +51,6 @@ def test_interfaces(capfd: Capture, faker: Faker) -> None:
         f"Missing rapydo/adminer:{__version__} image, add {colors.RED}--pull{colors.RESET} option",
     )
 
-    docker = Docker()
-
     # Launch Adminer UI with default port
     exec_command(
         capfd,
@@ -62,7 +59,6 @@ def test_interfaces(capfd: Capture, faker: Faker) -> None:
         # f"Creating {project_name}_adminer_run",
         "You can access Adminer interface on: http://localhost:7777",
     )
-    docker.client.container.remove("adminer", force=True, volumes=True)
 
     # Launch Adminer UI with custom port
     exec_command(
@@ -72,7 +68,6 @@ def test_interfaces(capfd: Capture, faker: Faker) -> None:
         # f"Creating {project_name}_adminer_run",
         "You can access Adminer interface on: http://localhost:123",
     )
-    docker.client.container.remove("adminer", force=True, volumes=True)
 
     # Launch Swagger UI with default port
     exec_command(
@@ -81,7 +76,6 @@ def test_interfaces(capfd: Capture, faker: Faker) -> None:
         "Pulling image for swaggerui...",
         "You can access SwaggerUI web page here: http://localhost:7777",
     )
-    docker.client.container.remove("swaggerui", force=True, volumes=True)
 
     # Launch Swagger UI with custom port
     exec_command(
@@ -89,7 +83,6 @@ def test_interfaces(capfd: Capture, faker: Faker) -> None:
         "run swaggerui --port 124",
         "You can access SwaggerUI web page here: http://localhost:124",
     )
-    docker.client.container.remove("swaggerui", force=True, volumes=True)
 
     # Test Swagger UI and Admin in production mode
     exec_command(
@@ -104,11 +97,9 @@ def test_interfaces(capfd: Capture, faker: Faker) -> None:
         "--prod run swaggerui --port 125",
         "You can access SwaggerUI web page here: https://localhost:125",
     )
-    docker.client.container.remove("swaggerui", force=True, volumes=True)
 
     exec_command(
         capfd,
         "--prod run adminer --port 126",
         "You can access Adminer interface on: https://localhost:126",
     )
-    docker.client.container.remove("adminer", force=True, volumes=True)
