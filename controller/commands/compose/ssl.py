ef8a155364fef838097fa4984cd5cc350cfe5689
Mattia <m.dantonio81@gmail.com>
2021-10-22 21:55:13 +0200

    Replaced compose v1 based exec command in ssl with new docker based command

--- controller/commands/compose/ssl.py
+++ controller/commands/compose/ssl.py
@@ -1,4 +1,3 @@
-import sys
 from pathlib import Path
 from typing import Optional
 
@@ -8,6 +7,7 @@ from controller import RED, log, print_and_exit
 from controller.app import Application, Configuration
 from controller.deploy.builds import verify_available_images
 from controller.deploy.compose import Compose
+from controller.deploy.docker import Docker
 
 # 0 0 * * 3 cd /home/??? && \
 #     COMPOSE_INTERACTIVE_NO_CLI=1 /usr/local/bin/rapydo ssl --no-tty > \
@@ -81,56 +81,50 @@ def ssl(
         log.info("Unable to automatically perform the requested operation")
         log.info("You can execute the following commands by your-self:")
 
-        container = f"{Configuration.project}_{service}_1"
+        c = f"{Configuration.project}_{service}_1"
         letsencrypt_path = "/etc/letsencrypt/real"
         print("")
-        print(f"docker cp {chain_file} {container}:{letsencrypt_path}/fullchain1.pem")
-        print(f"docker cp {key_file} {container}:{letsencrypt_path}/privkey1.pem")
+        print(f"docker cp {chain_file} {c}:{letsencrypt_path}/fullchain1.pem")
+        print(f"docker cp {key_file} {c}:{letsencrypt_path}/privkey1.pem")
         print(f"rapydo shell {service} 'nginx -s reload'")
         print("")
 
         return
 
-    command = f"/bin/bash updatecertificates {Configuration.hostname}"
-
+    docker = Docker()
     dc = Compose(files=Application.data.files)
 
-    try:
-        if volatile:
-            # once migrated to v2 a publish=[(443, 443)] will be needed
-            dc.create_volatile_container(service, command=command)
-        else:
-            dc.exec_command(service, user="root", command=command, disable_tty=no_tty)
-    except SystemExit as e:
-
-        if not volatile:
-            log.info(
+    command = f"/bin/bash updatecertificates {Configuration.hostname}"
+    if volatile:
+        # once migrated to v2 a publish=[(443, 443)] will be needed
+        dc.create_volatile_container(service, command=command)
+    else:
+        container = docker.get_container(service, slot=1)
+        if not container:
+            print_and_exit(
                 "If your proxy container is not running, try with {command}",
                 command=RED("rapydo ssl --volatile"),
             )
-        sys.exit(e.code)
-    else:
+        docker.exec_command(container, user="root", command=command, tty=not no_tty)
 
-        running_containers = dc.get_running_containers(Configuration.project)
-        if "neo4j" in running_containers:
-            # This is not true!! A full restart is needed
-            # log.info("Neo4j is running, but it will reload the certificate by itself")
-            # But not implemented yet...
-            log.info("Neo4j is running, a full restart is needed. NOT IMPLEMENTED YET.")
+    running_containers = dc.get_running_containers(Configuration.project)
+    if "neo4j" in running_containers:
+        # This is not true!! A full restart is needed
+        # log.info("Neo4j is running, but it will reload the certificate by itself")
+        # But not implemented yet...
+        log.info("Neo4j is running, a full restart is needed. NOT IMPLEMENTED YET.")
 
-        if "rabbit" in running_containers:
-            log.info(
-                "RabbitMQ is running, executing command to refresh the certificate"
-            )
-            # Please note that Erland is able to automatically reload the certificate
-            # But RabbitMQ does not. Probably in the future releases this command will
-            # No longer be required. To test it after the creation of the new cert:
-            #   echo -n | openssl s_client -showcerts -connect hostname:5671
-            # Please note that this command can fail if RabbitMQ is still starting
-            dc.exec_command(
-                "rabbit",
-                command="/usr/local/bin/reload_certificate",
-                disable_tty=no_tty,
-            )
+    if "rabbit" in running_containers:
+        log.info("RabbitMQ is running, executing command to refresh the certificate")
+        # Please note that Erland is able to automatically reload the certificate
+        # But RabbitMQ does not. Probably in the future releases this command will
+        # No longer be required. To test it after the creation of the new cert:
+        #   echo -n | openssl s_client -showcerts -connect hostname:5671
+        # Please note that this command can fail if RabbitMQ is still starting
+        dc.exec_command(
+            "rabbit",
+            command="/usr/local/bin/reload_certificate",
+            disable_tty=no_tty,
+        )
 
-        log.info("New certificate successfully enabled")
+    log.info("New certificate successfully enabled")
