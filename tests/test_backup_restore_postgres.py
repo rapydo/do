b10b65e09b4f4573c1e59c7dd94f0d5fef11840c
Mattia <m.dantonio81@gmail.com>
2021-11-09 12:25:50 +0100

    # Test fix

--- tests/test_backup_restore_postgres.py
+++ tests/test_backup_restore_postgres.py
@@ -241,8 +241,8 @@ def test_all(capfd: Capture, faker: Faker) -> None:
 
     exec_command(
         capfd,
-        "restart",
-        "Stack restarted",
+        "start",
+        "Stack started",
     )
 
     # Here we test the restore procedure:
