e2f2c0dded734636f3605a5ab2dc9d871243f8fe
Mattia D'Antonio <m.dantonio81@gmail.com>
2021-11-22 14:50:28 +0100

    Bump python-on-whales to 0.32.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Remember to update mypy.additional_dependencies
     install_requires=[
         "docker-compose==1.29.2",
-        "python-on-whales==0.31.0",
+        "python-on-whales==0.32.0",
         "python-dateutil",
         "pytz",
         "loguru",
