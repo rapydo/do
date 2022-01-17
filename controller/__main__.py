c6a09022b06229c0e151f7bc5a55abe0c879fe47
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-01-17 09:37:26 +0100

    # Removed debug code

--- controller/__main__.py
+++ controller/__main__.py
@@ -3,27 +3,15 @@ def main() -> None:
     # All imports moved here to prevent to slow down the import of main
     import warnings
 
-    from colorama import deinit, init
-    from python_on_whales.utils import DockerException
-
     from controller import TESTING, log, print_and_exit
 
     if TESTING:
-        warnings.simplefilter("always", Warning)
-        # Convert warnings to exceptions
-        warnings.simplefilter("error", UserWarning)
-        warnings.simplefilter("error", DeprecationWarning)
-        warnings.simplefilter("error", SyntaxWarning)
-        warnings.simplefilter("error", RuntimeWarning)
-        warnings.simplefilter("error", FutureWarning)
-        warnings.simplefilter("error", PendingDeprecationWarning)
-        warnings.simplefilter("error", ImportWarning)
-        warnings.simplefilter("error", UnicodeWarning)
-        warnings.simplefilter("error", BytesWarning)
-        warnings.simplefilter("error", ResourceWarning)
-    else:
-        warnings.simplefilter("always", UserWarning)
-        warnings.simplefilter("always", DeprecationWarning)
+        warnings.filterwarnings("error")
+    else:  # pragma: no cover
+        warnings.filterwarnings("default")
+
+    from colorama import deinit, init
+    from python_on_whales.utils import DockerException
 
     from controller.app import Application
 
