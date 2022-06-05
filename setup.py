824c43ef41dbc03cc42ba3e775a612d2c3f74a2e
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-06-05 11:29:51 +0200

    Moved back dependencies to setup.py for workflow compatibilities

--- setup.py
+++ setup.py
@@ -2,4 +2,24 @@
 
 from setuptools import setup
 
-setup()
+setup(
+    install_requires=[
+        "python-on-whales==0.40.0",
+        "python-dateutil",
+        "pytz",
+        "loguru",
+        "jinja2",
+        "sultan==0.9.1",
+        "plumbum",
+        "glom",
+        "GitPython==3.1.27",
+        "PyYAML==6.0",
+        "pip>=21.3",
+        "requests>=2.6.1",
+        "typer[all]==0.4.1",
+        "click==8.1.2",
+        "zxcvbn",
+        "tabulate",
+        "packaging",
+    ]
+)
