b7ebe888aa554d3ea0bf47a943f15e2d6255d902
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-08-06 10:11:21 +0200

    # Relaxed neo4j wait time (faster after the removal of GDS)

--- tests/test_backup_restore_neo4j.py
+++ tests/test_backup_restore_neo4j.py
@@ -58,7 +58,7 @@ def test_all(capfd: Capture, faker: Faker) -> None:
     # This will initialize neo4j
     exec_command(capfd, "shell backend 'restapi init'")
 
-    time.sleep(25)
+    time.sleep(20)
     # Just some delay extra delay. restapi init alone not always is enough...
     if Configuration.swarm_mode:
         time.sleep(30)
@@ -198,7 +198,7 @@ def test_all(capfd: Capture, faker: Faker) -> None:
     exec_command(capfd, "start", "Stack started")
 
     # Just some delay extra delay, neo4j is a slow starter
-    time.sleep(25)
+    time.sleep(20)
 
     # Restore command
     exec_command(
@@ -260,7 +260,7 @@ def test_all(capfd: Capture, faker: Faker) -> None:
     files.sort()
     neo4j_dump_file = files[-1]
 
-    time.sleep(25)
+    time.sleep(20)
 
     # Here we test the restore procedure:
     # 1) verify some data in the database
