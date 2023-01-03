5b3d1f00fd9ba0b97e3d63acc5857cde087e9989
Mattia D'Antonio <m.dantonio81@gmail.com>
2023-01-03 16:00:06 +0100

    # Syntax fix

--- controller/builds/backend-legacy310/docker-entrypoint.sh
+++ controller/builds/backend-legacy310/docker-entrypoint.sh
@@ -80,7 +80,7 @@ else
 
         if [[ ! -d "${PROJECT_NAME}/migrations" ]]; then
             echo "Skipping migrations check, ${PROJECT_NAME}/migrations does not exist";
-        elif [[ $(HOME=$CODE_DIR su -p ${APIUSER} -c 'alembic current" 2>&1 | tail -1 | grep "head"') ]]; then
+        elif [[ $(HOME=$CODE_DIR su -p ${APIUSER} -c 'alembic current 2>&1 | tail -1 | grep "head"') ]]; then
             echo "All database migrations are already installed";
         else
             HOME=$CODE_DIR su -p ${APIUSER} -c 'restapi wait'
