bdc52019d1cfba5114cbcae317a4c9b009fab97e
Mattia D'Antonio <m.dantonio81@gmail.com>
2023-07-04 23:12:04 +0200

    Authorized the installation of docker script with checksum beca7796f8eb377ac0089619675e63a4

--- controller/packages.py
+++ controller/packages.py
@@ -27,7 +27,7 @@ from sultan.api import Sultan  # type: ignore
 from controller import RED, log, print_and_exit
 
 # https://get.docker.com
-EXPECTED_DOCKER_SCRIPT_MD5 = "9e9bc39e56ca4780dbb00d03cba99bdc"
+EXPECTED_DOCKER_SCRIPT_MD5 = "beca7796f8eb377ac0089619675e63a4"
 
 # https://github.com/docker/compose/releases
 COMPOSE_VERSION = "v2.9.0"
