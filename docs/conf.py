09337b7e05d709184aac5a794667fa3e58a153b3
Mattia D'Antonio <m.dantonio81@gmail.com>
2024-04-28 12:03:06 +0200

    Bump RAPyDo to 3.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "RAPyDo"
 author = "RAPyDo"
 
 # The full version, including alpha/beta/rc tags
-release = "3.0"
+release = "3.1"
 
 
 # -- General configuration ---------------------------------------------------
